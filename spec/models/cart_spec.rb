46f1e6e00a24e959cd1846096f3c0be3abbbf231
alkp <apasenko@gmail.com>
2017-12-28 19:58:22 +0300

    Добавил тестирование связи с Product

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -4,4 +4,5 @@ RSpec.describe Cart, type: :model do
   it { should validate_presence_of(:guid) }
   it { should validate_presence_of(:status) }
   it { should have_many(:orders) }
+  it { should have_many(:products) }
 end
